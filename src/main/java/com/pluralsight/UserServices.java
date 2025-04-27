ccb806a1d8481fe4faa4400fb5a2afa3def80aeb
Eyob Mengistu <emengistu17@gmail.com>
2025-04-27 16:10:07 -0500

    Complete Ledger Screen and Exit Screen

--- src/main/java/com/pluralsight/UserServices.java
+++ src/main/java/com/pluralsight/UserServices.java
@@ -9,7 +9,8 @@ public class UserServices {
 
     // Displays the Home Screen menu
     public static void displayHomeScreen() {
-        System.out.println("\n=== Home Screen ===");
+        printSeparator("Welcome to Accounting Ledger App");
+        System.out.println("Please choose an option by typing the letter followed by a parentheses:\n");
         System.out.println("D) Add Deposit");
         System.out.println("P) Make Payment");
         System.out.println("L) View Ledger");
@@ -17,19 +18,44 @@ public class UserServices {
         System.out.print("Enter your choice:");
     }
 
+    // Displays the Ledger Screen menu
+    public static void ledgerScreen() {
+        printSeparator("Ledger Screen");
+        System.out.println("Please choose an option by typing the letter followed by a parentheses:\n");
+        System.out.println("A) All");
+        System.out.println("D) Deposits");
+        System.out.println("P) Payments");
+        System.out.println("R) Reports");
+        System.out.println("H) Home");
+        System.out.print("Enter your choice:");
+
+        String ledgerChoice = getScanner().nextLine().trim().toUpperCase();
+    }
+
+    // Exit Screen
+    public static void exit() {
+        printSeparator("Exiting...");
+        UserServices.pause(1000);
+        System.out.println("Thank you for using the Accounting Ledger App. Goodbye");
+
+    }
+
     // Prints a visual separator line
-    public static void printSeparator() {
-        System.out.println("-----------------------------------");
+    public static void printSeparator(String title) {
+        System.out.println("\n========================================================================");
+        System.out.print("                          " + title);
+        System.out.println("\n========================================================================");
+
     }
 
     // Returns the static Scanner object
-    public static Scanner getScanner(){
+    public static Scanner getScanner() {
         return myScanner;
     }
 
     // Pause the program for a given number of milliseconds
-    public static void pause(int milliseconds){
-        try{
+    public static void pause(int milliseconds) {
+        try {
             Thread.sleep(milliseconds);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
